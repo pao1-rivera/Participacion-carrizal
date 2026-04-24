21685d23d4346b345df905b186d4fa4a66dbe576
NoSoyCrisman <cristhianb397@gmail.com>
2026-04-24 13:27:40 +0000

    Supabase Added

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,15 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: localStorage,
+    storageKey: 'sb-auth-token',
+    detectSessionInUrl: false,
+    autoRefreshToken: true,
+    persistSession: true,
+    lock: false,
+  },
+});
